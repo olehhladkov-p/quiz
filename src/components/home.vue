8e1fd39f5fb11ab5b111eeb7ec7812f6b80af0a2
olehhladkov <oleh.hladkov@gmail.com>
2021-12-26 00:43:23 +0200

    Add quiz form

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,5 +2,6 @@
   <div>
     <h1 class="text-center">Welcome to quiz app</h1>
     <p>Quiz app description</p>
+    <router-link :to="{ name: 'quiz-form' }">Go to quiz form</router-link>
   </div>
 </template>
