78ba85b6dfef557d2e35922ed7d2053a0f798f23
olehhladkov <43970746+olehhladkov@users.noreply.github.com>
2021-12-26 00:44:45 +0200

    Add quiz form (#2)

---
+++ src/components/quiz/form.vue
@@ -0,0 +1,201 @@
+<template>
+  <div>
+    <h1 class="text-center">Setup your quiz!</h1>
+    <div class="quiz-form-wrapper">
+      <form
+        @change="onFormChange"
+        @submit.prevent="onFormSubmit"
+        class="quiz-form"
+      >
+        <p class="quiz-form-note">
+          <span v-show="processing"> Fetching form data... </span>
+        </p>
+
+        <select
+          v-model="category"
+          :disabled="processing || !categories.length"
+          name="category"
+          required
+        >
+          <option value="" disabled>
+            {{ categories.length ? 'Select a category' : loadingPlaceholder }}
+          </option>
+          <option
+            v-for="category in categories"
+            :key="category.id"
+            :value="category.id"
+          >
+            {{ category.name }}
+          </option>
+        </select>
+
+        <select
+          v-model="difficulty"
+          :disabled="processing || !category"
+          name="difficulty"
+          required
+        >
+          <option value="" disabled>Select a category difficulty</option>
+          <option
+            v-for="level in difficultyLevelList"
+            :key="level"
+            :value="level"
+          >
+            {{ level }}
+          </option>
+        </select>
+
+        <input
+          v-model="questions"
+          :disabled="processing || !difficulty"
+          :max="maxQuestionsAmount"
+          name="questions_amount"
+          placeholder="Select a number of questions"
+          type="number"
+          min="1"
+          required
+        />
+
+        <button class="btn" :disabled="isFormDisabled">Start a quiz</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  data() {
+    return {
+      loadingPlaceholder: 'Loading categories...'
+    }
+  },
+  created() {
+    this.setCategories()
+  },
+  computed: {
+    ...mapGetters([
+      'processing',
+      'quizStarted',
+      'categoryId',
+      'categories',
+      'difficultyLevel',
+      'difficultyLevelList',
+      'questionsAmount',
+      'maxQuestionsAmount'
+    ]),
+    isFormDisabled() {
+      return !this.category || !this.difficulty || !this.questions
+    },
+    category: {
+      get() {
+        return this.categoryId
+      },
+      set(value) {
+        this.setCategoryId(value)
+      }
+    },
+    difficulty: {
+      get() {
+        return this.difficultyLevel
+      },
+      set(value) {
+        this.setDifficultyLevel(value)
+      }
+    },
+    questions: {
+      get() {
+        return this.questionsAmount
+      },
+      set(value) {
+        return this.setQuestionsAmount(Number(value))
+      }
+    }
+  },
+  methods: {
+    ...mapActions([
+      'setQuizStarted',
+      'setCategoryId',
+      'setCategories',
+      'setDifficultyLevel',
+      'setQuestionsAmount',
+      'setMaxQuestionsAmount'
+    ]),
+    onFormChange(e) {
+      const targetName = e.target.name
+
+      if (
+        this.category &&
+        this.difficulty &&
+        targetName !== 'questions_amount'
+      ) {
+        this.setMaxQuestionsAmount()
+      }
+    },
+    onFormSubmit() {
+      this.setQuizStarted()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.quiz-form-wrapper {
+  display: flex;
+  justify-content: center;
+  max-width: 30rem; /* 480px */
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 1rem;
+}
+
+.quiz-form {
+  position: relative;
+  width: 100%;
+  padding: 2.5rem 3rem 1.5rem;
+  border-radius: 1rem;
+  border: 1px solid #999;
+}
+
+.quiz-form-note {
+  position: absolute;
+  top: 1rem;
+  margin: 0;
+  color: #999;
+}
+
+.quiz-form select:disabled,
+.quiz-form input:disabled {
+  opacity: 0.7;
+}
+
+.quiz-form select,
+.quiz-form input,
+.quiz-form .btn {
+  margin: 0.5rem 0;
+  padding: 0.5rem 1rem;
+  width: 100%;
+  line-height: normal;
+  border: 1px solid #ccc;
+  border-radius: 0.5rem;
+  background-color: #fff;
+}
+
+.quiz-form select option {
+  text-transform: capitalize;
+}
+
+.quiz-form .btn {
+  font-size: 1.125rem;
+  cursor: pointer;
+}
+
+.quiz-form .btn:not([disabled]):hover {
+  background-color: #eee;
+}
+
+.quiz-form .btn[disabled] {
+  cursor: not-allowed;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '@/components/home.vue'
+import QuizForm from '@/components/quiz/form.vue'
 
 Vue.use(VueRouter)
 
-const routes = [{ path: '/', name: 'home', component: Home }]
+const routes = [
+  { path: '/', name: 'home', component: Home },
+  { path: '/quiz-form', name: 'quiz-form', component: QuizForm }
+]
 
 export default new VueRouter({
   routes
